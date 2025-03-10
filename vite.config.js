648fde2225ad8aa32d0beefcbcd86f3d85ba49ea
Ole <144427985+Ole12345678910@users.noreply.github.com>
2025-03-10 18:15:13 +0100

    ok
    ok

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    // Spesifiser hvor byggede filer skal plasseres
-    outDir: 'dist',
+    // Specify the directory where the build files will be placed
+    outDir: 'dist', // This makes sure the output files go into the 'dist' folder
   },
   server: {
-    host: '0.0.0.0', // Dette gjør at du kan få tilgang fra andre enheter på nettverket
-    port: 3000, // Forsikre deg om at dette er riktig port
+    host: '0.0.0.0', // Allows access from any device on the network
+    port: 3000, // Port number (make sure this is correct when running locally)
   },
 })
